a87106bec7d7c7b3dab02fead228027798cee2fc
Josh Kahn <joshua.a.kahn@gmail.com>
2024-09-26 21:47:06 -0500

    (fix) optimized image output dir

--- next.config.mjs
+++ next.config.mjs
@@ -34,7 +34,7 @@ const nextConfig = {
     nextImageExportOptimizer_exportFolderPath: "out",
     nextImageExportOptimizer_quality: "75",
     nextImageExportOptimizer_storePicturesInWEBP: "true",
-    nextImageExportOptimizer_exportFolderName: "optimages",
+    // nextImageExportOptimizer_exportFolderName: "optimages",
     nextImageExportOptimizer_generateAndUseBlurImages: "true",
     nextImageExportOptimizer_remoteImageCacheTTL: "0",
   }
