9a56b40b4cdcc3ce563e3ee95e5d4828f3c753ae
Josh Kahn <joshua.a.kahn@gmail.com>
2026-02-15 16:47:45 -0600

    fix: local dev

--- astro.config.ts
+++ astro.config.ts
@@ -4,18 +4,25 @@ import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 
+function reactDomEdgeAlias() {
+  return {
+    name: 'react-dom-server-edge',
+    apply: 'build' as const,
+    config: () => ({
+      resolve: {
+        alias: { 'react-dom/server': 'react-dom/server.edge' },
+      },
+    }),
+  }
+}
+
 export default defineConfig({
   site: 'https://blog.iamjkahn.com',
   output: 'server',
   adapter: cloudflare({ imageService: 'compile' }),
   integrations: [react(), sitemap()],
   vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "react-dom/server": "react-dom/server.edge",
-      },
-    },
+    plugins: [tailwindcss(), reactDomEdgeAlias()],
   },
   redirects: {
     '/archive': '/posts',
