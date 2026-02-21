72e6ed555af3455f0c887c08b41a5c49f243b974
Josh Kahn <joshua.a.kahn@gmail.com>
2026-02-21 15:49:54 -0600

    fix: move bsky login

--- src/lib/bluesky.ts
+++ src/lib/bluesky.ts
@@ -1,19 +1,7 @@
-import { Agent, CredentialSession } from "@atproto/api";
+import { Agent, CredentialSession } from "@atproto/api"
 
-const session = new CredentialSession(new URL("https://bsky.social"));
-
-try {
-  await session.login({
-    identifier: import.meta.env.BLUESKY_HANDLE,
-    password: import.meta.env.BLUESKY_APP_PASSWORD,
-  });
-  console.log("🦋 Bluesky login successul");
-} catch (err) {
-  console.warn("⚠️ Bluesky login failed");
-  console.warn(err);
-  throw err;
+export async function createAgent(handle: string, password: string) {
+  const session = new CredentialSession(new URL("https://bsky.social"))
+  await session.login({ identifier: handle, password })
+  return new Agent(session)
 }
-
-const agent = new Agent(session);
-
-export default agent;
